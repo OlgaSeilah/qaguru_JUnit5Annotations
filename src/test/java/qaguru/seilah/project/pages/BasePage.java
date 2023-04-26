486ac216ffc4bd9519a76ece65dcb9f5df8f912d
seilah <princenoru@gmail.com>
2023-04-26 23:07:39 +0400

    parametrizedTests: common locator moved to BasePage

--- src/test/java/qaguru/seilah/project/pages/BasePage.java
+++ src/test/java/qaguru/seilah/project/pages/BasePage.java
@@ -1,6 +1,11 @@
 package qaguru.seilah.project.pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public abstract class BasePage {
+    protected final SelenideElement acceptCookiesBtn = $("[class*='styles_cookieAccept'] button");
 
     public abstract BasePage openEngVersion();
 }
